edbabfd42a75f51b31492c523737d764b05a5946
madebyjonny <44024764+madebyjonny@users.noreply.github.com>
2020-11-07 15:49:01 +0000

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
+
 import React from "react";
 
 const Index: React.FC = () => {
-  return <h1>hello</h1>;
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  return {
+    props: {},
+    revalidate: 1,
+  };
 };
 
 export default Index;
